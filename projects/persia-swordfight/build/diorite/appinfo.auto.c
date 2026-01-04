b397edc6c5b9b299abc92ed2898774f74c426c27
Priyankar Kumar <priyankar.kumar98@gmail.com>
2026-01-04 09:21:03 +0530

    sword-fights

---
+++ projects/persia-swordfight/build/diorite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Persia Swordfight",
+  .company = "Pebble Developer",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_DIORITE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x1B, 0x1A, 0x08, 0x09, 0x93, 0x12, 0x4C, 0x89, 0x8E, 0xF7, 0x9F, 0x66, 0x87, 0xB9, 0xA5, 0x62 },
+  .virtual_size = 0xb6b6
+};
